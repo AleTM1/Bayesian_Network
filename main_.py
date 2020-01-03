52d06a75f0879d4cc825722ea17dc3540cdf198e
AleTM1 <alessandro.maggio98@gmail.com>
2020-01-03 18:44:08 +0100

    1.4 local files

--- main_.py
+++ main_.py
@@ -5,9 +5,9 @@ from dataset_generator import dataset_gen as generator, csv_to_numpy
 def main():
     n = 10000
     t = 8
-    states_path = '/home/alessandro/Documenti/IA/Datasets/Asia/states.csv'
-    prob_table_path = '/home/alessandro/Documenti/IA/Datasets/Asia/prob.csv'
-    structure_path = '/home/alessandro/Documenti/IA/Datasets/Asia/structure.csv'
+    states_path = 'resources/states.csv'
+    prob_table_path = 'resources/prob.csv'
+    structure_path = 'resources/structure.csv'
     data = csv_to_numpy.csv_to_numpy(states_path, prob_table_path, structure_path)
     dataset = generator.dataset_gen(data[0], data[1], data[2], n)
     print("Dataset generato")
